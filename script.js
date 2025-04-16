842911de99f8c30287614906e637b3dbfec78de0
Nandhu1216 <srinandhu1216@gmail.com>
2025-04-16 10:05:06 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    document.getElementById("formStatus").textContent = "✅ Message sent (frontend only)";
+  });
+  
